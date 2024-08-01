1e887f26bdd0f08094cbd851409e1219abd59489
folujam <olusway@gmail.com>
2024-08-01 16:01:34 +0100

    annotated return

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ patterns = {
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
-                 separator: str):
+                 separator: str) -> str:
     """method returns the log message obfuscated"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
