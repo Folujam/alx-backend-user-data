1a5240d4d593dee24194b69f3828d561709bb563
folujam <olusway@gmail.com>
2024-08-01 22:34:47 +0100

    t2 test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """regex-ing Module"""
+import logging
 import re
 from typing import List
 
@@ -17,3 +18,23 @@ def filter_datum(fields: List[str],
     """method returns the log message obfuscated"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init inherited and feilds"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records"""
+        messag = super(RedactingFormatter, self).format(record)
+        line = filter_datum(self.fields, self.REDACTION, messag, self.SEPARATOR)
+        return line
