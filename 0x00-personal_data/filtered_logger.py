baef5f44b2a2a50cc9870856021e43698b7f1ab8
folujam <olusway@gmail.com>
2024-08-01 15:55:16 +0100

    INIT AND T1 TEST

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""regex-ing Module"""
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str):
+    """method returns the log message obfuscated"""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
