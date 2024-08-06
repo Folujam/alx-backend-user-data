87c48443e1f6e6dd93c57f8c16d188be93cb2fe3
folujam <olusway@gmail.com>
2024-08-06 23:49:33 +0100

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,17 +3,16 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """this is the auth class obj"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth method"""
         pass
 
-
     def authorization_header(self, request=None) -> str:
         """auth header method"""
         return request
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """finds current user"""
