b3148a96de2612a312e0df50b82240600fbadcad
folujam <olusway@gmail.com>
2024-08-10 23:18:37 +0100

    pycodestyle good

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,13 +5,15 @@ Auth = __import__('auth').Auth
 
 class BasicAuth(Auth):
     """this class inherits api/v1/auth/auth.py Auth class as base
-    
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
+
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
 
         :param authorization_header: The Authorization header value
         :return: The Base64 encoded string, or None if invalid
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
