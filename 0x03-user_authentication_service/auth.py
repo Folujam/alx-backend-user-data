14ae1d04b924d3cb9d73d69e9bb578ab724a43d5
folujam <olusway@gmail.com>
2024-08-14 10:16:39 +0100

    auth: register user, db: sqengine; echo=false

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """authentication Module"""
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +16,19 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(byte, salt)
     return hash
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(email: str, password: str) -> User:
+        """registers user if they dont exist"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
