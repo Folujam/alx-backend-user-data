deb40ddf23cb9a30193d86e2b26166a908d052eb
Folujam <olusway@gmail.com>
2024-08-15 20:37:19 +0100

    7post done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """the app Module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', method=["GET"], strict_slashes=False)
 def index() -> str:
     """employs the get mthod at root"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", method=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users returns a JSON payload"""
+    #unpack gotten values of email && password
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
