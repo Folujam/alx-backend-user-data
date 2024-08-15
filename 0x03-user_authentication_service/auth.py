f9108700e64e7c272338a5e16b04c4f1c40a60e7
Folujam <olusway@gmail.com>
2024-08-15 14:18:19 +0100

    self defined in reg_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,7 +26,7 @@ class Auth:
         """ initializes Auth class"""
         self._db = DB()
 
-    def register_user(email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """registers user if they dont exist"""
         try:
             self._db.find_user_by(email=email)
