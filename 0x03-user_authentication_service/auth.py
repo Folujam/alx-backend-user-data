f557b54026ed1b3f1dfb2128e528a3161a7603c2
folujam <olusway@gmail.com>
2024-08-17 14:50:28 +0100

    val_log meth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,14 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validates if password of user matches"""
+        try:
+            user = self._db.find_user_by(email)
+            if user is not None:
+                return bcrypt.checkpw(password.encode('utf-8'),
+                                      user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
