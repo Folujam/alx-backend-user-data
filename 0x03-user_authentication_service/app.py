3c878976541fa69d13801f75170aa799f3985b87
folujam <olusway@gmail.com>
2024-08-18 16:19:55 +0100

    test /sess[post] #0

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """the app Module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 from auth import Auth
 
@@ -25,6 +25,18 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """post /sessions, returns acc login payload"""
+    email, password = request.form.get("email"), request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
