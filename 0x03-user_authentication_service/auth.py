23fc799652201c33dd7625efd2c0e0c643b645a9
folujam <olusway@gmail.com>
2024-08-18 22:12:50 +0100

    jdj

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,15 @@ class Auth:
         if user_id:
             self._db.update_user(user_id=user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """get reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        uu = _generate_uuid()
+        self._db.update_user(User.id, reset_token=uu)
+        return uu
